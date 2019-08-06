cfd36cdebf946ce1a7e59b4180c372fd2f74a441
ivan.salivon <ivan.salyvon@kyivstar.net>
2019-08-06 18:06:26 +0300

    design collection and repository 'profile'

---
+++ persistence-microservice/src/main/java/io/newage/persistence/domain/Profile.java
@@ -0,0 +1,34 @@
+package io.newage.persistence.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "profile")
+public class Profile implements Serializable {
+
+    private static final long serialVersionUID = -4827087355941026678L;
+
+    @Id
+    @NotBlank
+    private String uuid;
+
+    @NotBlank
+    @Field
+    private String email;
+
+    @NotBlank
+    @Field
+    private String password;
+}
---
+++ persistence-microservice/src/main/java/io/newage/persistence/repository/ProfileRepository.java
@@ -0,0 +1,9 @@
+package io.newage.persistence.repository;
+
+import io.newage.persistence.domain.Profile;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends MongoRepository<Profile, String> {
+}
