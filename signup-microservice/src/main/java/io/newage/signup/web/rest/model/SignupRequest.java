fb592dcfc9a511accb1986a544c9acf6db3a7da7
ivan.salivon <ivan.salyvon@kyivstar.net>
2019-08-07 12:41:06 +0300

    override toString 'SignupRequest'

--- signup-microservice/src/main/java/io/newage/signup/web/rest/model/SignupRequest.java
+++ signup-microservice/src/main/java/io/newage/signup/web/rest/model/SignupRequest.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString(of = {"uuid", "email"})
 public class SignupRequest implements Serializable {
 
     @ApiModelProperty(hidden = true)
