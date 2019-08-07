8361b0dc56aba688d041a3836b0c4e71795d26eb
ivan.salivon <ivan.salyvon@kyivstar.net>
2019-08-07 12:40:37 +0300

    override toString 'Profile'

--- persistence-microservice/src/main/java/io/newage/persistence/domain/Profile.java
+++ persistence-microservice/src/main/java/io/newage/persistence/domain/Profile.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -16,6 +17,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "profile")
+@ToString(of = "uuid")
 public class Profile implements Serializable {
 
     private static final long serialVersionUID = -4827087355941026678L;
